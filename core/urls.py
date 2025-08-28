b848d9571cc9e517dea8958efa1d279bb507fb1b
Misael Benitez <misa.benitez@iosoffices.com>
2025-08-28 15:03:45 -0600

    Se añadio urls para el login con diseño e independiente

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+
+@login_required
+def home(_):
+    return HttpResponse("¡Estás logueado!<form type='POST' action='/accounts/logout'> <button>Salir</button>")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", lambda request: redirect("login/")),  # Redirige "/" a "/login/"
-    path("", include("inventory.urls")),  # Incluye tus rutas de la app
+    # path("", lambda request: redirect("login/")),  # Redirige "/" a "/login/"
+    path("", home, name='home'),  # Incluye tus rutas de la app
+    path("accounts/", include("accounts.urls"))
 ]
