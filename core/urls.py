d12a1e28f8ae36ea168911562c852cc4b48422ee
Abiud Benitez <abiudmisael10@hotmail.com>
2025-08-27 23:59:45 -0600

    Add cambios a gitignore

--- core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.shortcuts import redirect
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", lambda request: redirect("login/")),  # Redirige "/" a "/login/"
-    path("", include("inventory.urls")),  # Incluye tus rutas de la app
-]
