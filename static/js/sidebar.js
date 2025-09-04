ed3a7eceda1ddce4f0bab6b82dbfa89801a6815c
Abiud Benitez <abiudmisael@hotmail.com>
2025-09-03 23:08:27 -0600

    Se añadio la opcion de hacer el tema dark o light

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -1,9 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll(".submenu-toggle").forEach(btn => {
+  document.querySelectorAll(".submenu-toggle").forEach((btn) => {
     btn.addEventListener("click", () => {
       const parent = btn.closest(".has-submenu");
       const isOpen = parent.classList.toggle("open");
       btn.setAttribute("aria-expanded", isOpen);
     });
+  });
+  const themeToggle = document.getElementById("theme-toggle");
+  themeToggle.addEventListener("click", () => {
+    const currentTheme = document.documentElement.classList.contains("light");
+    const newTheme = currentTheme === "light" ? "" : "light";
+    document.documentElement.classList.add(newTheme);
+    document.documentElement.classList.remove(currentTheme ? "light" : "");
+    localStorage.setItem("theme", newTheme);
+    themeToggle.innerHTML =
+      newTheme === "light"
+        ? '<i class="fa-solid fa-sun"></i>'
+        : '<i class="fa-solid fa-moon"></i>';
   });
 });
