13bb718a27db09d91417c94cd8dda021b2565907
Abiud Benitez <abiudmisael@hotmail.com>
2025-09-03 22:39:37 -0600

    Se cambio la delaracion de los nombres de las apps

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from .views import SignInView, SignOutView, prueba
 
-app_name = 'accounts'
-
 urlpatterns = [
     path('login/', SignInView.as_view(), name='login'),
     path('logout/', SignOutView.as_view(), name='logout'),
