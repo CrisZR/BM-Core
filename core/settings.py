fcc22c040063228ae5cccea168dc3320fc2cb709
Abiud Benitez <abiudmisael@hotmail.com>
2025-09-03 21:46:28 -0600

    Se añadio el navigation para usarlo de forma global

--- core/settings.py
+++ core/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "livereload",
+    "navigation",
     "inventory",
     "accounts",
 ]
@@ -67,6 +68,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "navigation.context_processors.menu_context"
             ],
         },
     },
