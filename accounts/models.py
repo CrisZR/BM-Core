016b0c415d1095b594f0da6061f43814f53f1c1f
Abiud Benitez <abiudmisael@hotmail.com>
2025-09-06 18:05:28 -0600

    Se añadio modelo para guardar el tema de preferencia del usuario

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
+    theme_preference = models.CharField(max_length=20, choices=[('light', 'Light'), ('dark', 'Dark')], default='light')
+
+    def __str__(self):
+        return f"{self.user.username} - {self.theme_preference}"
