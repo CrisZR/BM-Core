3fa83fd8b9732b5d5adfeaef146c05417934347d
Abiud Benitez <abiudmisael@hotmail.com>
2025-09-21 01:41:48 -0600

    Se añadio dependencias para el MEDIA

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.shortcuts import render
 from django.urls import path, include
 
@@ -8,3 +10,6 @@ urlpatterns = [
     path("accounts/", include(("accounts.urls", "accounts"), namespace="accounts")),
     path("inventory/", include(("inventory.urls", "inventory"), namespace="inventory"))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
