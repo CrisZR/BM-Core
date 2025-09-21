85172b58840eaf91312d18a015586952a4d8dcb3
Abiud Benitez <abiudmisael@hotmail.com>
2025-09-21 04:06:20 -0600

    Se añadio funcion para la validacion de la sesion

--- accounts/context_processors.py
+++ accounts/context_processors.py
@@ -1,6 +1,13 @@
+from django.conf import settings
+
 def theme_context(request):
     if request.user.is_authenticated:
         profile = getattr(request.user, "userprofile", None)
         if profile:
             return {"theme_preference": profile.theme_preference}
     return {"theme_preference": None}
+
+def session_timeout(request):
+    return {
+        "session_age": settings.SESSION_COOKIE_AGE,
+    }
