03ece580cc22113132134d36c802113646055d37
Abiud Benitez <abiudmisael@hotmail.com>
2025-09-21 01:41:29 -0600

    Se añadio app para el control de form y dependencias de media para subir imagenes

--- core/settings.py
+++ core/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "livereload",
+    "crispy_forms",
     "home",
     "navigation",
     "inventory",
@@ -143,3 +144,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
