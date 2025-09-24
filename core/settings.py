11885cae37131933b903d18fb732fe5d58830074
ClaudioRobles21 <claudioroger2013@gmail.com>
2025-09-23 20:45:20 -0600

    Imagen del Logo en el Login y comentario de prueba para cambio en el setting.py

--- core/settings.py
+++ core/settings.py
@@ -143,3 +143,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+#COMMIIT TEST
