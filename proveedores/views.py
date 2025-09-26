10ec64371caef6836031b70cbba13f7225ee0802
Misael Benitez <misa.benitez@iosoffices.com>
2025-09-26 10:43:53 -0600

    Se añadio funcionalidad para el add del proveedor

--- proveedores/views.py
+++ proveedores/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
 from .forms import AddProveedorForm, ContactoProveedorFormSet
+from django.db import transaction
+from django.contrib import messages
 
 # Create your views here.
 def add(request):
-    form = AddProveedorForm()
-    formset = ContactoProveedorFormSet()
+
+    if request.method == "POST":
+        form = AddProveedorForm(request.POST, request.FILES)
+        formset = ContactoProveedorFormSet(request.POST)
+        if form.is_valid() and formset.is_valid():
+            try:
+                with transaction.atomic():
+                    proveedor = form.save(commit=False)
+                    proveedor.usuario_creo = request.user
+
+                    contactos = formset.save(commit=False)
+                    for contacto in contactos:
+                        contacto.proveedor = proveedor
+                        contacto.usuario_creo = request.user
+                        contacto.save()
+
+                    messages.success(request, "Proveedor guardado correctamente")
+            except Exception as e:
+                messages.error(request, f"Ha habido un problema al guardar el proveedor: {e}" )
+        else:
+            messages.error(request, "Hubo un problema al enviar el formulario.")
+    else:
+        form = AddProveedorForm()
+        formset = ContactoProveedorFormSet()
     return render(request, 'add_edit.html', {'form': form, 'formset': formset})
