31464c8cfaadfa028ef60304ed712e46b1ab81f9
Abiud Benitez <abiudmisael@hotmail.com>
2025-09-25 23:22:26 -0600

    Se añadio el inicio del add de proveedores

--- proveedores/views.py
+++ proveedores/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .forms import AddProveedorForm, ContactoProveedorFormSet
 
 # Create your views here.
+def add(request):
+    form = AddProveedorForm()
+    formset = ContactoProveedorFormSet()
+    return render(request, 'add_edit.html', {'form': form, 'formset': formset})
