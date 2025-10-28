2dd8c8c9fbaed4e7c32a99661b967ed8bc01731f
Abiud Benitez <abiudmisael10@hotmail.com>
2025-10-28 14:46:15 -0600

    cambios para poder hacer la alta de los pdfs

--- proveedores/forms.py
+++ proveedores/forms.py
@@ -3,6 +3,11 @@ from .models import Proveedor, ContactoProveedor
 from django.forms.widgets import ClearableFileInput
 from django.forms import inlineformset_factory
 
+
+class CustomClearableFileInput(ClearableFileInput):
+    template_name = "forms/widgets/custom_clearable_file_input.html"
+
+
 class AddProveedorForm(forms.ModelForm):
     class Meta:
         model = Proveedor
@@ -15,62 +20,88 @@ class AddProveedorForm(forms.ModelForm):
             "codigo_postal",
             "caratula",
             "opinion_de_cumplimiento",
-            "direccion"
+            "direccion",
         ]
         widgets = {
             "nombre": forms.TextInput(
-                attrs={"class": "form-control", "placeholder": "Nombre del proveedor", "autocomplete": "off"}
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Nombre del proveedor",
+                    "autocomplete": "off",
+                }
             ),
             "numero_de_cuenta": forms.TextInput(
-                attrs={"class": "form-control", "placeholder": "Número de cuenta", "autocomplete": "off"}
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Número de cuenta",
+                    "autocomplete": "off",
+                }
             ),
-            "opinion_de_cumplimiento": ClearableFileInput(
+            "opinion_de_cumplimiento": CustomClearableFileInput(
                 attrs={"class": "form-control"}
             ),
             "razon_social": forms.TextInput(
-                attrs={"class": "form-control", "placeholder": "Razón social", "autocomplete": "off"}
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Razón social",
+                    "autocomplete": "off",
+                }
             ),
             "rfc": forms.TextInput(
-                attrs={"class": "form-control", "placeholder": "RFC", "autocomplete": "off"}
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "RFC",
+                    "autocomplete": "off",
+                }
             ),
-            "regimen_fiscal": forms.Select(
-                attrs={"class": "form-select"}
-            ),
+            "regimen_fiscal": forms.Select(attrs={"class": "form-select"}),
             "direccion": forms.TextInput(
                 attrs={"class": "form-control", "placeholder": "Dirección"}
             ),
             "codigo_postal": forms.TextInput(
-                attrs={"class": "form-control", "placeholder": "Código postal", "autocomplete": "off"}
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Código postal",
+                    "autocomplete": "off",
+                }
             ),
-            "caratula": ClearableFileInput(
-                attrs={"class": "form-control"}
-            ),
+            "caratula": CustomClearableFileInput(attrs={"class": "form-control"}),
         }
-        
+
+
 class AddContactoProveedorForm(forms.ModelForm):
     class Meta:
         model = ContactoProveedor
-        fields = [
-            "nombre",
-            "email",
-            "telefono"
-        ]
+        fields = ["nombre", "email", "telefono"]
         widgets = {
             "nombre": forms.TextInput(
-                attrs={"class": "form-control", "placeholder": "Nombre del contacto", "autocomplete": "off"}
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Nombre del contacto",
+                    "autocomplete": "off",
+                }
             ),
             "email": forms.EmailInput(
-                attrs={"class": "form-control", "placeholder": "Correo electrónico", "autocomplete": "off"}
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Correo electrónico",
+                    "autocomplete": "off",
+                }
             ),
             "telefono": forms.TextInput(
-                attrs={"class": "form-control", "placeholder": "Teléfono", "autocomplete": "off"}
-            )
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Teléfono",
+                    "autocomplete": "off",
+                }
+            ),
         }
-        
+
+
 ContactoProveedorFormSet = forms.inlineformset_factory(
     Proveedor,
     ContactoProveedor,
     form=AddContactoProveedorForm,
     extra=1,
-    can_delete=True
+    can_delete=True,
 )
