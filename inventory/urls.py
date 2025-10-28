c6d1315a1c5a3936c4ce15a0b0a0f65cfae5d957
Abiud Benitez <abiudmisael10@hotmail.com>
2025-10-28 14:51:26 -0600

    formateo del doc

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from .views import *
 
-app_name="inventory"
+app_name = "inventory"
 
 urlpatterns = [
-  path('add/', add, name="add"),
-  path('productos/', productos, name="productos"),
-  path('productos/edit/<int:pk>/', edit, name="edit"),
+    path("add/", add, name="add"),
+    path("productos/", productos, name="productos"),
+    path("productos/edit/<int:pk>/", edit, name="edit"),
 ]
