bb1397ebb0a062e960a22c68953bacff473aac1b
Abiud Benitez <abiudmisael10@hotmail.com>
2025-10-28 14:50:50 -0600

    cambios en el js de proveedores index para la tabla

--- proveedores/static/js/proveedores/index.js
+++ proveedores/static/js/proveedores/index.js
@@ -1,6 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
     const columns = [
-        { name: "ID", id: "id", editable: false, width: 50 },
+        {
+          name: "",
+          id: "acciones",
+          editable: false,
+          width: "50px",
+          format: (value, row, column, data) => {
+            return `
+              <a href="/proveedores/edit/${data.id}/"><i class="fa-solid fa-pen-to-square"></i></a>
+            `;
+          }
+        },
+        { name: "ID", id: "id", editable: false },
         { name: "Nombre", id: "nombre" },
         { name: "Razón Social", id: "razon_social" },
         { name: "RFC", id: "rfc" },
@@ -8,14 +19,15 @@ document.addEventListener("DOMContentLoaded", () => {
         { name: "Régimen Fiscal", id: "regimen_fiscal" },
         { name: "Código Postal", id: "codigo_postal" },
       ];
-  
+
       new DataTable("#tablaProveedores", {
         columns: columns,
         data: proveedores,
-        // layout: "fluid",
-        // responsive: true,
-        // sortable: true,
-        // inlineFilters: true,
-        // pagination: true,
+        layout: "fluid",
+        serialNoColumn: false,
+        responsive: true,
+        sortable: true,
+        inlineFilters: true,
+        pagination: true,
       });
 })
