484d1f3f2fa317258d3fa2034a13bf1531d5c03b
Abiud Benitez <abiudmisael10@hotmail.com>
2025-10-28 14:52:40 -0600

    config para django

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/static/",
+  // base: "/static/",
   build: {
-    outDir: "../static/dist",
-    emptyOutDir: true
+    outDir: 'dist', // por defecto, pero puede cambiar si quieres
   },
   plugins: [react()],
 })
