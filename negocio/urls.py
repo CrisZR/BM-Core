b4baa5c00dc1dc7ec30b6ca4215ecb11930b2ad2
Abiud Benitez <abiudmisael10@hotmail.com>
2025-10-29 08:42:46 -0600

    inicio del modulo de negocios

---
+++ negocio/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import *
+
+app_name = "negocio"
+
+urlpatterns = [
+    # path("", index, name="index"),
+    path("add/", add, name="add"),
+    # path("edit/<int:pk>/", edit, name="edit"),
+]
--- negocio/views.py
+++ negocio/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def add(request):
+    return render(request, "negocio/add_edit.html")
