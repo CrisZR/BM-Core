e26d94e6ede9532477aaa17f40412064177a51ee
Abiud Benitez <abiudmisael10@hotmail.com>
2025-10-30 16:48:09 -0600

    Se crearon los formularios que se usaran en la creacion de negocios

---
+++ negocio/forms.py
@@ -0,0 +1,124 @@
+from django import forms
+from .models import Negocio, ContactoNegocio
+from django.forms.widgets import ClearableFileInput
+
+
+class CustomClearableFileInput(ClearableFileInput):
+    template_name = "widgets/custom_clearable_file_input.html"
+
+
+class addNegocioForm(forms.ModelForm):
+    class Meta:
+        model = Negocio
+        fields = [
+            "nombre",
+            "nombre_fiscal",
+            "direccion",
+            "telefono",
+            "email",
+            "logo",
+            "rfc",
+            "regimen_fiscal",
+            "codigo_postal",
+        ]
+        widgets = {
+            "nombre": forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Nombre del negocio",
+                    "autocomplete": "off",
+                }
+            ),
+            "nombre_fiscal": forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Nombre fiscal",
+                    "autocomplete": "off",
+                }
+            ),
+            "direccion": forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Dirección",
+                    "autocomplete": "off",
+                }
+            ),
+            "telefono": forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Teléfono",
+                    "autocomplete": "off",
+                }
+            ),
+            "email": forms.EmailInput(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Email",
+                    "autocomplete": "off",
+                }
+            ),
+            "logo": forms.ClearableFileInput(
+                attrs={"class": "form-control", "placeholder": "Logo del negocio"}
+            ),
+            "rfc": forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "RFC",
+                    "autocomplete": "off",
+                }
+            ),
+            "regimen_fiscal": forms.Select(attrs={"class": "form-select"}),
+            "codigo_postal": forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Código postal",
+                    "autocomplete": "off",
+                }
+            ),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["regimen_fiscal"].empty_label = "Selecciona un régimen fiscal"
+
+
+class addContactoForm(forms.ModelForm):
+    class Meta:
+        model = ContactoNegocio
+        fields = [
+            "nombre",
+            "telefono",
+            "email",
+        ]
+        widgets = {
+            "nombre": forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Nombre del contacto",
+                    "autocomplete": "off",
+                }
+            ),
+            "telefono": forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Teléfono",
+                    "autocomplete": "off",
+                }
+            ),
+            "email": forms.EmailInput(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Email",
+                    "autocomplete": "off",
+                }
+            ),
+        }
+
+
+ContactoNegocioFormSet = forms.inlineformset_factory(
+    Negocio,
+    ContactoNegocio,
+    form=addContactoForm,
+    extra=1,
+    can_delete=True,
+)
