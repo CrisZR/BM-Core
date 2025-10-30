75b1f79d10eaaa3cdd7dac8283a8077b7b0676c3
Abiud Benitez <abiudmisael10@hotmail.com>
2025-10-30 16:47:48 -0600

    Se añadio el codigo para los forms

--- negocio/views.py
+++ negocio/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from .forms import addNegocioForm, ContactoNegocioFormSet
 
 # Create your views here.
 
 
 def add(request):
-    return render(request, "negocio/add_edit.html")
+    if request.method == "POST":
+        form = addNegocioForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            # Redirect or do something after successful form submission
+    else:
+        form = addNegocioForm()
+        setform = ContactoNegocioFormSet()
+    return render(request, "negocio/add_edit.html", {"form": form, "setform": setform})
