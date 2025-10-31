8d1ae41c3f6416f58f9fcbba947ea2a700d67291
Abiud Benitez <abiudmisael10@hotmail.com>
2025-10-30 20:15:50 -0600

    inicio de la funcionalidad para contactos de negocios

---
+++ negocio/static/negocio/js/add_edit.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const addButton = document.getElementById("addContacto");
+  const deleteButton = document.getElementById("deleteContacto");
+  const divContactos = document.getElementById("contContactos");
+  const totalForms = document.getElementById("id_contactos-TOTAL_FORMS");
+});
