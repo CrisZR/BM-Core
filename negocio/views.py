d1cc95ca192c7f4c9ad2313e234e4712fd04d960
Abiud Benitez <abiudmisael10@hotmail.com>
2025-10-30 20:14:27 -0600

    Se hizo el backend para las vistas de negocio

--- negocio/views.py
+++ negocio/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import addNegocioForm, ContactoNegocioFormSet
+from django.db import transaction
+from django.contrib import messages
+from .models import Negocio
 
 # Create your views here.
 
@@ -7,10 +10,73 @@ from .forms import addNegocioForm, ContactoNegocioFormSet
 def add(request):
     if request.method == "POST":
         form = addNegocioForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
+        formset = ContactoNegocioFormSet(request.POST)
+        if form.is_valid() and formset.is_valid():
+            try:
+                with transaction.atomic():
+                    negocio = form.save(commit=False)
+                    negocio.usuario_creo = request.user
+                    negocio.save()
+
+                    contactos = formset.save(commit=False)
+                    for contacto in contactos:
+                        contacto.negocio = negocio
+                        contacto.usuario_creo = request.user
+                        contacto.save()
+
+                    messages.success(request, "Negocio guardado correctamente")
+                    return redirect("negocio:add")
+            except Exception as e:
+                messages.error(
+                    request, f"Ha habido un problema al guardar el negocio: {e}"
+                )
+        else:
+            messages.error(request, "Hubo un problema al enviar el formulario.")
             # Redirect or do something after successful form submission
     else:
         form = addNegocioForm()
-        setform = ContactoNegocioFormSet()
-    return render(request, "negocio/add_edit.html", {"form": form, "setform": setform})
+        formset = ContactoNegocioFormSet()
+    return render(request, "negocio/add_edit.html", {"form": form, "formset": formset})
+
+
+def edit(request, pk):
+    negocio = Negocio.objects.get(pk=pk)
+    contactos_iniciales = negocio.contactos.count()
+
+    if request.method == "POST":
+        form = addNegocioForm(request.POST, request.FILES, instance=negocio)
+        formset = ContactoNegocioFormSet(request.POST, instance=negocio)
+        if form.is_valid() and formset.is_valid():
+            try:
+                with transaction.atomic():
+                    negocio = form.save(commit=False)
+                    negocio.usuario_modifico = request.user
+                    negocio.save()
+
+                    contactos = formset.save(commit=False)
+                    for contacto in contactos:
+                        if not contacto.pk:
+                            contacto.usuario_creo = request.user
+                        contacto.negocio = negocio
+                        contacto.save()
+
+                    for contacto in formset.deleted_objects:
+                        contacto.delete()
+
+                    messages.success(request, "Negocio modificado correctamente")
+                    return redirect("negocio:add")
+            except Exception as e:
+                messages.error(
+                    request, f"Ha habido un problema al modificar el negocio: {e}"
+                )
+        else:
+            messages.error(request, "Hubo un problema al enviar el formulario.")
+    else:
+        form = addNegocioForm(instance=negocio)
+        formset = ContactoNegocioFormSet(instance=negocio)
+    return render(request, "negocio/add_edit.html", {"form": form, "formset": formset})
+
+
+def index(request):
+    negocios = Negocio.objects.select_related("regimen_fiscal").all()
+    return render(request, "negocio/index.html", {"negocios": list(negocios)})
