5d366e8d8204c7faf7f4fedc7e9e27abf03586ff
Abiud Benitez <abiudmisael10@hotmail.com>
2025-11-25 08:17:19 -0600

    Se añadieron urls para las ordenes de compra

--- compra/urls.py
+++ compra/urls.py
@@ -1,8 +1,20 @@
 from django import urls
 from .views import *
+from . import views
 
 app_name = "compra"
 
 urlpatterns = [
+    urls.path("", index, name="index"),
     urls.path("add/", add, name="add"),
+    urls.path("view/<int:orden_id>/", view, name="view"),
+    urls.path("findProductos/", views.findProductos, name="findProductos"),
+    urls.path("findNegocioRFC/", views.findNegocioRFC, name="findNegocioRFC"),
+    urls.path(
+        "autorizar_od/<int:orden_id>/", views.autorizar_oden, name="autorizar_od"
+    ),
+    urls.path("rechazar_od/<int:orden_id>/", views.rechazar_orden, name="rechazar_od"),
+    urls.path(
+        "completar_orden/<int:orden_id>/", views.completar_orden, name="completar_orden"
+    ),
 ]
